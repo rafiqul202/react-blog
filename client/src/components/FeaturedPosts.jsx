8ff88c98059e281c1031eec06fe59a588a169ea6
Rafiqul Hasan <rafiqulhasan398@gmail.com>
2026-09-07 21:31:17 +0600

    admin fetured way FeaturedPosts post render ui modern  beautiful then first items render

--- client/src/components/FeaturedPosts.jsx
+++ client/src/components/FeaturedPosts.jsx
@@ -1,33 +1,57 @@
-
 import ImageKit from "./ImageKit";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import VideoKit from "./VideoKit";
+import { format } from "timeago.js";
+const fetchFeatured = async () => {
+  const res = await axios.get(
+    `${import.meta.env.VITE_API_URL}/posts?featured=true&limit=4&sort=newest`,
+  );
+  return res.data;
+};
 const FeaturedPosts = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["featuredPosts"],
+    queryFn: () => fetchFeatured(),
+  });
+
+  if (isPending) return "Loading...";
+  if (error) return "Something went wrong";
+  const post = data?.posts;
+  if (!post || post.length === 0) return;
+
+  console.log("featuredPost fetch data", post);
   return (
     <div className="mt-8 flex flex-col lg:flex-row gap-8">
       {/* first post */}
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* image */}
-        <ImageKit
-          src={`/Blogs/public/featured1.jpeg`}
-          alt="featured1"
-          className="rounded-lg object-cover"
-          w="895"
-        />
+        {post[0]?.img ? (
+          <ImageKit
+            src={post[0].img}
+            alt="featured1"
+            className="rounded-lg object-cover"
+            w="895"
+          />
+        ) : (
+          <VideoKit src={post[0].video} />
+        )}
         {/* details */}
         <div className="flex items-center gap-4">
           <h1 className="font-semibold">01.</h1>
-          <Link className="text-blue-800">Web Design</Link>
-          <span className="text-gray-400 text-sm">2 days ago</span>
+          <Link className="text-blue-800">{post[0].category}</Link>
+          <span className="text-gray-400 text-sm">
+            {format(post[0].createdAt)}
+          </span>
         </div>
         {/* title */}
 
         <Link
-          to={"/test"}
+          to={post[0].slug}
           className="text-base lg:text-xl font-semibold lg:font-bold"
         >
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio,
-          aperiam.
+          {post[0].title}
         </Link>
       </div>
       {/* others */}
